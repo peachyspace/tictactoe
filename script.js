32543a52d87c559cb6c61c5b630ed32a6e52616c
Priscila <priscilapintado66@gmail.com>
2023-06-14 19:04:43 -0400

    created circles and crosses on board

--- script.js
+++ script.js
@@ -4,8 +4,15 @@ const createBoard = function () {
   intialCells.forEach((cell, index) => {
     const cellElement = document.createElement("div");
     cellElement.classList.add("square");
+    cellElement.id = index;
+    cellElement.addEventListener("click", addGo);
     board.append(cellElement);
   });
 };
 
 createBoard();
+function addGo(e) {
+  const goDisplay = document.createElement("div");
+  goDisplay.classList.add("cross");
+  e.target.append(goDisplay); //the event target property returns the HTML element that triggered an event
+} //function declarations are hoisted completely to the top
