8a2f50326d93501c5e1c7fb63a5d7ef30278128d
Priscila <priscilapintado66@gmail.com>
2023-06-15 10:09:15 -0400

    setting up score tracker

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const board = document.querySelector("#board");
+const infoDisplay = document.querySelector("#info");
 const intialCells = ["", "", "", "", "", "", "", "", ""];
 const createBoard = function () {
   intialCells.forEach((cell, index) => {
@@ -9,10 +10,38 @@ const createBoard = function () {
     board.append(cellElement);
   });
 };
-
+let go = "circle";
+infoDisplay.textContent = "Circle goes first";
 createBoard();
 function addGo(e) {
   const goDisplay = document.createElement("div");
-  goDisplay.classList.add("cross");
+  goDisplay.classList.add(go);
   e.target.append(goDisplay); //the event target property returns the HTML element that triggered an event
-} //function declarations are hoisted completely to the top
+  go = go === "circle" ? "cross" : "circle";
+  infoDisplay.textContent = `it is ${go}'s turn!!!`;
+  e.target.removeEventListener("click", addGo); //we can only add 1 item to a square
+  updatingScore();
+} //function declarations are hoisted cßmpletely to the top
+function updatingScore() {
+  const allSquares = document.querySelectorAll(".square");
+  const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  winningCombos.forEach((array) => {
+    const circleWins = array.every((cell) =>
+      allSquares[cell].firstChild?.classList.contains("circle")
+    );
+  });
+  if (circleWins) {
+    infoDisplay.textContent = "Circle is the winner!!!";
+    allSquares.forEach(square.replaceWith(square.cloneNode(true)));
+    return;
+  }
+}
